cb587dfb79b26fc96a539b0d25642756b484f9e2
Edfrancys <edfrancys.victor@outlook.com>
2021-02-12 16:24:28 -0300

    layout and elements

--- pages/index.js
+++ pages/index.js
@@ -1,82 +1,39 @@
 import Head from 'next/head'
 
 export default function Home() {
-  return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 px-20 text-center">
-        <h1 className="text-6xl font-bold">
-          Welcome to{' '}
-          <a className="text-blue-600" href="https://nextjs.org">
-            Next.js!
-          </a>
-        </h1>
+    function menuTogle(){
+        document.getElementById('menuTogle').classList.toggle('hidden')
+    }
 
-        <p className="mt-3 text-2xl">
-          Get started by editing{' '}
-          <code className="p-3 font-mono text-lg bg-gray-100 rounded-md">
-            pages/index.js
-          </code>
-        </p>
+    return (
+        <>
+            <Head>
+                <title>Agência Vejartes</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-        <div className="flex flex-wrap items-center justify-around max-w-4xl mt-6 sm:w-full">
-          <a
-            href="https://nextjs.org/docs"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Documentation &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Find in-depth information about Next.js features and API.
-            </p>
-          </a>
+            <header className='w-screen py-1 bg-gray-800 flex justify-center'>
+                <nav className='flex justify-between w-full sm:w-9/12 px-8' >
+                    <div className='text-white'>Logo</div>
+                    <div className='text-white hidden sm:block' >Home - Sobre - Portifólio - Contato</div>
+                    <div className='text-white hidden sm:block'>Contato</div>
+                    <div className='sm:hidden' >
+                        <button onClick={menuTogle} className='px-2 text-white border border-white'>H</button>  
+                    </div>
+                </nav>
+                
+            </header>
+            <div id='menuTogle' className='w-full flex justify-center hidden'>
+                <div className='w-96 bg-red-600 text-center'>
+                    <div>Home</div>
+                    <div>Home</div>
+                    <div>Portifólio</div>
+                    <div>Contato</div>
+                </div>
+            </div>
 
-          <a
-            href="https://nextjs.org/learn"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Learn &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Learn about Next.js in an interactive course with quizzes!
-            </p>
-          </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Examples &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Discover and deploy boilerplate example Next.js projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="p-6 mt-6 text-left border w-96 rounded-xl hover:text-blue-600 focus:text-blue-600"
-          >
-            <h3 className="text-2xl font-bold">Deploy &rarr;</h3>
-            <p className="mt-4 text-xl">
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className="flex items-center justify-center w-full h-24 border-t">
-        <a
-          className="flex items-center justify-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
-      </footer>
-    </div>
-  )
+        </>
+    )
 }
